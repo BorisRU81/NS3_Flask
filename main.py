8c3a91b0d3e02d0a70d58ef86b2407a8467469d5
Boris <Hrom80@bk.ru>
2023-07-10 12:01:45 +0300

    Две страницы со ссылками туда, сюда

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Privet!'
+    return 'Адмирал! <br><a href="/slogan"> Слоган <a/>'
+
+@app.route('/slogan')
+def slogan ():
+    return 'Ибо крепка, как смерть, любовь!<br><a href = "/"> Назад</a>'
+
+@app.route('/countdown')   #Декоратор. Функции пишем через декоратор.
+def countdown():
+    lst=[str(x) for x in range(10, 0, -1)]
+    lst.append('Start!!! puuuffffff')
+    return '<br>'.join(lst)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
 
-я
