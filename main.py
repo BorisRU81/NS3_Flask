e40b7f12cb1565923d29ef754a16c8e35b858b53
Boris <Hrom80@bk.ru>
2023-07-10 12:23:44 +0300

    И вот мы добавили картинку в код HTML

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-from flask import Flask #какркас вариант Flask приложения
+from flask import Flask, url_for #какркас вариант Flask приложения
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Адмирал! <br><a href="/slogan"> Слоган <a/>'
+    return 'Адмирал! <br><a href="/slogan"> А слоган к фильму есть ? <a/>'
 
 @app.route('/slogan')
 def slogan ():
-    return 'Ибо крепка, как смерть, любовь!<br><a href = "/"> Назад</a>'
+    return 'Ибо крепка, как смерть, любовь!<br><a href = "/"> А какое было название фильма? </a>'
 
 @app.route('/countdown')   #Декоратор. Функции пишем через декоратор.
 def countdown():
     lst=[str(x) for x in range(10, 0, -1)]
     lst.append('Start!!! puuuffffff')
     return '<br>'.join(lst)
+
+@app.route('/poster')  # Добавление картинки через HTML
+def poster ():
+    return f"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Poster</title>
+</head>
+<body>
+<h1>Постер к фильму</h1>
+<img src="{url_for('static', filename='images/Japan.jpg')}"
+alt="Тут должна была быть картинка но она не прогрузилась. Печаль.">
+
+<p> И спать охота, что п*****</p>
+</body>
+</html>"""
 
 
 if __name__ == '__main__':
