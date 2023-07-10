fa83bdde15bfcba0069bce2850c341325c93ba9d
Boris <Hrom80@bk.ru>
2023-07-10 11:11:32 +0300

    Стартовый вариант приложения на FLASK

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask #какркас вариант Flask приложения
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return 'Privet!'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
+
